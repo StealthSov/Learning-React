a9b3859a748d2ec27d9a7dd7f46b9fe5fc52c1d7
StealthSov <50644368+StealthSov@users.noreply.github.com>
2021-09-15 14:11:45 -0400

    Finished Clock with States

--- intro.js
+++ intro.js
@@ -1,16 +1,33 @@
 //Creating a clock by re-rendering the entire Clock element every second. (Very slow)
 
-function Clock(props) {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {props.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
+class Clock extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()};
+    }
+    componentDidMount(){
+        this.timerID = setInterval(()=>this.tick(), 1000);
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+    tick() {
+        this.setState({
+          date: new Date()
+        });
+      }
+    render() {
+        return (
+          <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+          </div>
+        );
+      }
 }
 
 function tick(){
-    ReactDOM.render(<Clock date={new Date()}/>, document.body);
+    ReactDOM.render(<Clock/>, document.body);
 }
 
 setInterval(tick, 1000);
